e9326cdf257d530f3ad070879d8fa1abe5ea519a
sanket chavan <csaket016@gmail.com>
2023-04-20 19:13:50 +0530

    Create spearation with custom hook

---
+++ src/Hooks/useGames.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGameFromApi {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [errors, setErrors] = useState('');
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGameFromApi>('/games', { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setErrors(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, errors };
+};
+
+export default useGames;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,27 +1,8 @@
-import { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
 import { Text } from '@chakra-ui/react';
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGameFromApi {
-  count: number;
-  results: Game[];
-}
+import useGames from '../Hooks/useGames';
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [errors, setErrors] = useState('');
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGameFromApi>('/xgames')
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setErrors(err.message));
-  });
+  const { games, errors } = useGames();
 
   return (
     <>
